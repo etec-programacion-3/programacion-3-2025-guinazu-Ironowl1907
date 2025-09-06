28940192a840a9ebbf5fe45a7e21f582097b5b7d
Facundo <facuguialtana7@gmail.com>
2025-09-06 17:04:39 -0300

    Home screen

--- workout_logger/lib/main.dart
+++ workout_logger/lib/main.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-// import 'package:flutter/services.dart';
+import 'package:workout_logger/screens/home.dart';
 
 void main() => runApp(const MyApp());
 
@@ -8,30 +8,7 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      // Root widget
-      home: Scaffold(
-        appBar: AppBar(title: const Text('My Home Page')),
-        body: Center(
-          child: Builder(
-            builder: (context) {
-              return Column(
-                children: [
-                  const Text('Hello, World!'),
-                  const SizedBox(height: 20),
-                  ElevatedButton(
-                    onPressed: () {
-                      print('Click!');
-                    },
-                    child: const PaddedText(),
-                  ),
-                ],
-              );
-            },
-          ),
-        ),
-      ),
-    );
+    return MaterialApp(debugShowCheckedModeBanner: false, home: HomePage());
   }
 }
 

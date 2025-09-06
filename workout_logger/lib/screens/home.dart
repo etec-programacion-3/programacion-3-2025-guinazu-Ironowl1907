1f386d30da820464dba1015cf19c75e6e8d07279
Facundo <facuguialtana7@gmail.com>
2025-09-06 18:02:48 -0300

    Basic Nav Bar

--- workout_logger/lib/screens/home.dart
+++ workout_logger/lib/screens/home.dart
@@ -1,10 +1,52 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_svg/flutter_svg.dart';
 
-class HomePage extends StatelessWidget {
+class HomePage extends StatefulWidget {
   const HomePage({super.key});
 
   @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  int _currentIndex = 0;
+
+  @override
   Widget build(BuildContext context) {
-    return Scaffold(appBar: AppBar(title: Text("Home")));
+    return Scaffold(
+      bottomNavigationBar: BottomNavigationBar(
+        currentIndex: _currentIndex,
+        type: BottomNavigationBarType.fixed,
+        selectedItemColor: Colors.blue, // active color
+        unselectedItemColor: Colors.grey, // inactive color
+        onTap: (index) {
+          setState(() {
+            _currentIndex = index;
+          });
+        },
+        items: [
+          const BottomNavigationBarItem(icon: Icon(Icons.home), label: "Home"),
+          BottomNavigationBarItem(
+            icon: SvgPicture.asset(
+              'assets/Exercise.svg',
+              width: 24,
+              height: 24,
+              color: _currentIndex == 1 ? Colors.blue : Colors.grey,
+            ),
+            label: "Workout",
+          ),
+          BottomNavigationBarItem(
+            icon: SvgPicture.asset(
+              'assets/account.svg',
+              width: 24,
+              height: 24,
+              color: _currentIndex == 1 ? Colors.blue : Colors.grey,
+            ),
+            label: "Profile",
+          ),
+        ],
+      ),
+      appBar: AppBar(title: const Text("Home")),
+    );
   }
 }

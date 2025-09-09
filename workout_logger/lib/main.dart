fc450fb5debd29cdac698967997c7fb6597bc684
Facundo <facuguialtana7@gmail.com>
2025-09-09 08:18:40 -0300

    Fixed android db

--- workout_logger/lib/main.dart
+++ workout_logger/lib/main.dart
@@ -1,4 +1,6 @@
+import 'dart:io';
 import 'package:flutter/material.dart';
+import 'package:sqflite/sqflite.dart';
 import 'package:sqflite_common_ffi/sqflite_ffi.dart';
 import 'package:workout_logger/services/database_service.dart';
 import 'widgets/navigation_bar.dart';
@@ -6,14 +8,15 @@ import 'widgets/navigation_bar.dart';
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
 
-  sqfliteFfiInit();
-  databaseFactory = databaseFactoryFfi;
+  if (Platform.isLinux || Platform.isWindows || Platform.isMacOS) {
+    sqfliteFfiInit();
+    databaseFactory = databaseFactoryFfi;
+  }
 
-  runApp(const MyApp());
-
-  // initialize DB after UI starts
   final dbService = DatabaseService.instance;
   await dbService.database;
+
+  runApp(const MyApp());
 }
 
 class MyApp extends StatelessWidget {

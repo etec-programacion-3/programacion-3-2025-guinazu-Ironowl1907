1375604abe72ed74c6011faa74d51a22cff8f3ef
Ironowl1907 <facuguialtana7@gmail.com>
2025-10-10 09:01:23 -0300

    Starting workout page UI

--- lib/screens/workout.dart
+++ lib/screens/workout.dart
@@ -1,22 +1,50 @@
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import 'package:workout_logger/providers/routine_provider.dart';
+import 'package:workout_logger/widgets/workout_routine_card.dart';
 
-class WorkoutPage extends StatelessWidget {
+class WorkoutPage extends StatefulWidget {
   const WorkoutPage({super.key});
 
   @override
+  State<WorkoutPage> createState() => _WorkoutPageState();
+}
+
+class _WorkoutPageState extends State<WorkoutPage> {
+  @override
+  void initState() {
+    super.initState();
+    Future.microtask(() {
+      Provider.of<RoutineProvider>(context, listen: false).load();
+    });
+  }
+
+  @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Consumer<RoutineProvider>(
+      builder: (BuildContext context, RoutineProvider provider, _) {
+        return ListView.builder(
+          itemCount: provider.routines.length,
+          itemBuilder: (BuildContext context, int index) {
+            return Padding(
+              padding: const EdgeInsets.all(12.0),
+              child: WorkoutRoutineCard(routine: provider.routines[index]),
+            );
+          },
+        );
+      },
+    );
   }
 }
 
 PreferredSizeWidget workoutAppBar() {
   return AppBar(
-    title: const Text("Workout"),
+    title: const Text('Workout'),
     bottom: PreferredSize(
       preferredSize: const Size.fromHeight(1.0),
       child: Builder(
-        builder: (context) {
-          final dividerColor = Theme.of(context).dividerColor;
+        builder: (BuildContext context) {
+          final Color dividerColor = Theme.of(context).dividerColor;
           return Container(color: dividerColor, height: 1.0);
         },
       ),

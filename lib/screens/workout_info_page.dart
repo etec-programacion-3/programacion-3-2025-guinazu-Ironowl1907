73345ed874a6f2c5fd074783b7fe6817bd365aef
Ironowl1907 <facuguialtana7@gmail.com>
2025-10-18 18:30:52 -0300

    Starting with info page 2

--- lib/screens/workout_info_page.dart
+++ lib/screens/workout_info_page.dart
@@ -7,9 +7,54 @@ class WorkoutInfoPage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    final ThemeData theme = Theme.of(context);
     return Scaffold(
-      appBar: AppBar(title: Text(workout.title ?? 'Unamed Workout')),
-      body: Expanded(child: Container(color: Colors.red)),
+      appBar: AppBar(title: const Text('Wokrout Detail')),
+      body: Padding(
+        padding: const EdgeInsets.all(12.0),
+        child: ListView(children: <Widget>[infoCard(theme)]),
+      ),
+    );
+  }
+
+  Card infoCard(ThemeData theme) {
+    return Card(
+      color: theme.colorScheme.surfaceContainerLow,
+      child: Padding(
+        padding: const EdgeInsets.all(12.0),
+        child: Column(
+          spacing: 4,
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: <Widget>[
+            Text(workout.title ?? 'Unamed', style: theme.textTheme.titleLarge),
+            Text(
+              workout.endedAt.toString(),
+              style: theme.textTheme.titleMedium!.copyWith(color: Colors.grey),
+            ),
+            Row(
+              spacing: 8,
+              children: <Widget>[
+                infoChip('Duration', 'this is info', theme),
+                infoChip('Sets', 'this is info', theme),
+                infoChip('Volume', 'this is info', theme),
+              ],
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+
+  Widget infoChip(String title, String info, ThemeData theme) {
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: <Widget>[
+        Text(
+          title,
+          style: theme.textTheme.labelMedium!.copyWith(color: Colors.grey),
+        ),
+        Text(info),
+      ],
     );
   }
 }
